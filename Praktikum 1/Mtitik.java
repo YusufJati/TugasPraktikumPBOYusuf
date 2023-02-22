b18568a6f0c5730317297a417eb970caa0751036
YusufJati <88493069+YusufJati@users.noreply.github.com>
2023-02-22 20:37:07 +0700

    Update Mtitik.java

--- Praktikum 1/Mtitik.java
+++ Praktikum 1/Mtitik.java
@@ -4,6 +4,7 @@ public class Mtitik {
     public static void main(String[] args) {
         Titik t1 = new Titik();
         Titik t2 = new Titik();
+        Titik t3 = new Titik(5,6);
         t1.setAbsis(1);
         t1.setOrdinat(2);
         t2.setAbsis(3);
@@ -11,6 +12,7 @@ public class Mtitik {
         System.out.println("Jumlah objek titik: " + (int) Titik.getCounterTitik());
         System.out.println("Titik 1: (" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
         System.out.println("Titik 2: (" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
+        System.out.println("Titik 3: (" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
     }
 
 }
