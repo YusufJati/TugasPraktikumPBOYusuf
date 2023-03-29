5731d3f343cc41449f8c7f259102d50d19c2a831
YusufJati <88493069+YusufJati@users.noreply.github.com>
2023-03-29 14:20:37 +0700

    Add files via upload

---
+++ Praktikum 5/MBujurSangkar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MBujurSangkar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BujurSangkar bs = new BujurSangkar();
+        System.out.print("Masukkan sisi: ");
+        double sisi = scan.nextDouble();
+        System.out.println("Luas bujur sangkar dengan sisi " + sisi + " adalah " + bs.hitungLuas(sisi) + " satuan luas");
+        
+    }
+}
